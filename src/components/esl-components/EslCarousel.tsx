c82bef9906f1ddda2dda93e4a69686b39209ff33
sameerkindia <sameerkhanaa5@gmail.com>
2025-12-17 08:39:43 +0530

    Esl carousel updated

--- src/components/esl-components/EslCarousel.tsx
+++ src/components/esl-components/EslCarousel.tsx
@@ -4,6 +4,7 @@ import SliderCarousel from "../SliderCarousel";
 type ImageData = {
   name: string;
   src: string;
+  description? : string;
 };
 
 type CarouselData = {
@@ -20,13 +21,6 @@ function EslCarousel({ carouselData }: EslCarousel) {
   return (
     <section className="py-12">
       <div className="container mx-auto px-4 max-w-7xl relative">
-        <div className="mb-6">
-          <h2 className="text-2xl font-semibold text-center mb-4">
-            {carouselData.title}
-          </h2>
-
-          <p className="text-xl text-muted-foreground mx-auto text-center">{carouselData.desc}</p>
-        </div>
 
         <SliderCarousel
           data={carouselData.imageData}
